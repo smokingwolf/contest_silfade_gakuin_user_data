f1c06895a0888d191b555e46c39627099da033f1
SmokingWOLF <smokingwolf@silversecond.net>
2025-08-11 14:58:10 +0900

    upload

---
+++ SilGakuMOD/html/tdisjs.js
@@ -0,0 +1,8 @@
+function index(num){
+	var url = parent.lower.location.href;
+	var loc = url.indexOf("#");
+	if(loc != -1){
+		url = url.substring(0, loc);
+	}
+	parent.lower.location.href = url + "#" + num;
+}
